87da469e5c31e4e5b2980175d407896f84518b53
Cheri <avyishi@gmail.com>
2016-12-12 19:30:40 -0400

    Fixed stylesheet rendering issue

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var typing = false;
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
 	res.sendfile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('message', function(msg){
+    io.emit('message', msg);
   });
 });
+
 
 http.listen(3000, function() {
 	console.log("Listening on port 3000....");
